95c22574357951cafaeb7bf9a0a2d4f0501dff49
DaviDM-1 <daviimonteiroo14@gmail.com>
2026-02-20 16:28:23 -0300

    Cards de informação

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,6 +8,10 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     <!--Link do Bootstrap Icons-->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
+    <!--Fonte do Google Fonts-->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Noto+Sans+Chorasmian&family=Playwrite+CU+Guides&display=swap" rel="stylesheet">
     <!--Link do CSS-->
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     <title>Melanoma</title>
@@ -27,10 +31,10 @@
                 <div class="collapse navbar-collapse" id="navbarNav">
                     <ul class="navbar-nav ms-auto">
                         <li class="nav-item">
-                            <a class="nav-link active" href="#">Início</a>
+                            <a class="nav-link active" href="#home">Início</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Sobre</a>
+                            <a class="nav-link" href="#sobre">Sobre</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="#">Melanoma</a>
@@ -56,10 +60,39 @@
                         <h1>Identificação de Melanoma com Inteligência Artificial</h1>
                         <p class="lead">Sistema baseado em machine learning para auxiliar na detectação de lesões suspeitas de Melanoma</p>
                         <div class="d-flex gap-3 justify-content-center">
-                            <a class="btn btn-light btn-lg px-4" href="#"><i class="bi bi-camera me-2">Detector</i>
+                            <a class="btn btn-light btn-lg px-4" href="#"><i class="bi bi-camera me-2">Analisar</i>
                             </a>
                             <a href="#" class="btn btn-outline-light btn-lg px-4"> <i class="bi bi-info-circle me-2">Entender Melanoma</i>
                             </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+
+        <section id="Sobre" class="py-5">
+            <div class="container py-5">
+                <div class="row g-4">
+                    <div class="col-md-4">
+                        <div class="info-card">
+                            <i class="bi bi-cpu"></i>
+                            <h3>Machine learning</h3>
+                            <p>Redes neurais convolucionais treinadas com diversas imagens dermatoscópicas para reconhecer padrões característicos de melanoma</p>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="info-card">
+                            <i class="bi bi-clock-history"></i>
+                            <h3>Diagnóstico Precoce</h3>
+                            <p>O melanoma detectado em estágio inicial tem 90% de chance de cura. Nossa ferramenta auxilia na identificação rápida de lesões suspeitas</p>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="info-card">
+                            <i class="bi bi-graph-up"></i>
+                            <h3>Alguma coisa: talvez a precisão da IA</h3>
+                            <p>Nosso modelo alcançou tal resultado de precisão e tals</p>
                         </div>
                     </div>
                 </div>
